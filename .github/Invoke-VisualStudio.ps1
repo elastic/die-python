6473622e904e688a1e072aa3b117e723611193dd
Christophe Alladoum <christophe.alladoum@elastic.co>
2024-06-04 11:08:11 -0700

    invoke-vs missed vs2019

--- .github/Invoke-VisualStudio.ps1
+++ .github/Invoke-VisualStudio.ps1
@@ -11,18 +11,28 @@ Function Invoke-CmdScript {
     }
 }
 
+Function Invoke-VisualStudio2019x86
+{
+  Invoke-CmdScript "${env:ProgramFiles(x86)}/Microsoft Visual Studio/2019/Professional/VC/Auxiliary\Build/vcvars32.bat"
+}
+
+Function Invoke-VisualStudio2019x64
+{
+  Invoke-CmdScript "${env:ProgramFiles(x86)}/Microsoft Visual Studio/2019/Professional/VC/Auxiliary/Build/vcvars64.bat"
+}
+
 Function Invoke-VisualStudio2022win32 {
-    Invoke-CmdScript "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvars32.bat"
+    Invoke-CmdScript "${env:ProgramFiles}/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvars32.bat"
 }
 
 Function Invoke-VisualStudio2022x64 {
-    Invoke-CmdScript "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvars64.bat"
+    Invoke-CmdScript "${env:ProgramFiles}/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvars64.bat"
 }
 
 Function Invoke-VisualStudio2022arm64 {
-    Invoke-CmdScript "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvarsamd64_arm64.bat"
+    Invoke-CmdScript "${env:ProgramFiles}/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvarsamd64_arm64.bat"
 }
 
 Function Invoke-VisualStudio2022arm {
-    Invoke-CmdScript "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvarsamd64_arm.bat"
+    Invoke-CmdScript "${env:ProgramFiles}/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvarsamd64_arm.bat"
 }
