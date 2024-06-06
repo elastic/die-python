086ec0f338a8647954321729dada38b56b0b3667
Christophe Alladoum <christophe.alladoum@elastic.co>
2024-06-05 17:58:48 -0700

    make die build as static

--- cmake/FindDieLibrary.cmake
+++ cmake/FindDieLibrary.cmake
@@ -7,27 +7,22 @@ set(ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/..")
 set(QT_BUILD_VERSION "6.7.1")
 
 if(WIN32)
-  #
-  # Use aqtinstall to install Qt pre-compiled binaries only, ex:
-  #
-  # python -m aqt  install-qt windows desktop 6.7.1 win64_llvm_mingw
-  # or
   # python -m aqt  install-qt windows desktop 6.7.1 win64_msvc2019_64
-  #
   set(QT_BUILD_COMPILER "msvc2019_64")
   set(Qt6_DIR "${ROOT_DIR}/build/${QT_BUILD_VERSION}/${QT_BUILD_COMPILER}/lib/cmake/Qt6")
   set(QT_DIR ${Qt6_DIR})
   list(APPEND CMAKE_MODULE_PATH ${Qt6_DIR})
 endif(WIN32)
+# For Linux/MacOS, use the binaries from the system
 
 find_package(Qt6 REQUIRED COMPONENTS Core Qml Concurrent)
 
 FetchContent_Declare(
   DieLibrary
   GIT_REPOSITORY "https://github.com/calladoum-elastic/die_library"
-  GIT_TAG ff412022d34289115426ba1cb7b8663d728f7bb3
+  GIT_TAG af4528ba080ff06060dddd87b1f71bb7aa980af8
 )
-
+set(DIE_BUILD_AS_STATIC ON CACHE INTERNAL "")
 FetchContent_MakeAvailable( DieLibrary )
 
 message(STATUS "Using DieLibrary in '${dielibrary_SOURCE_DIR}'")
