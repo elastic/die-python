cbea84fe0073f5318812e111f9b6086179515df2
Christophe Alladoum <christophe.alladoum@elastic.co>
2025-02-20 16:42:57 -0800

    adjust cmake

--- cmake/FindDieLibrary.cmake
+++ cmake/FindDieLibrary.cmake
@@ -7,16 +7,26 @@ set(ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/..")
 set(QT_BUILD_VERSION "6.8.2")
 
 if(WIN32)
-  # python -m aqt install-qt -O build windows desktop ${QT_BUILD_VERSION} win64_msvc2019_64
-  set(QT_BUILD_COMPILER "msvc2019_64")
+  # python -m aqt install-qt -O build windows desktop ${QT_BUILD_VERSION} win64_msvc2022_64
+  set(QT_BUILD_COMPILER "msvc2022_64")
 elseif(LINUX)
-  # python -m aqt install-qt -O build linux desktop ${QT_BUILD_VERSION} gcc_64
-  set(QT_BUILD_COMPILER "gcc_64")
+  # TODO oversimplify : compilation HOST arch = TARGET arch
+  if ("${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "x86_64")
+    # python -m aqt install-qt -O build linux desktop ${QT_BUILD_VERSION} linux_gcc_64 (x64)
+    set(QT_BUILD_COMPILER "gcc_arm64")
+  endif()
+
+  if ("${CMAKE_HOST_SYSTEM_PROCESSOR}" STREQUAL "arm64")
+    # python -m aqt install-qt -O build linux_arm desktop ${QT_BUILD_VERSION} linux_gcc_arm64 (arm64)
+    set(QT_BUILD_COMPILER "gcc_arm64")
+  endif()
 elseif(APPLE)
   # python -m aqt install-qt -O build mac desktop ${QT_BUILD_VERSION} clang_64
   set(QT_BUILD_COMPILER "macos")
-else()
-  message(FATAL "nope")
+endif()
+
+if(NOT "${QT_BUILD_COMPILER}")
+  message(FATAL "Invalid Qt compiler setting")
 endif()
 
 set(Qt6_DIR "${ROOT_DIR}/build/${QT_BUILD_VERSION}/${QT_BUILD_COMPILER}/lib/cmake/Qt6")
