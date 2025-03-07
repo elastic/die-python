0cc17636a36d9d6edcca60b82634b6d571d574cd
Christophe Alladoum <christophe.alladoum@elastic.co>
2025-03-07 15:03:38 -0800

    update dielib

--- cmake/FindDieLibrary.cmake
+++ cmake/FindDieLibrary.cmake
@@ -28,7 +28,7 @@ find_package(Qt6 REQUIRED COMPONENTS Core Qml Concurrent)
 FetchContent_Declare(
   DieLibrary
   GIT_REPOSITORY "https://github.com/horsicq/die_library"
-  GIT_TAG 80e05f3dd3b4fdd287bee0d98d1f27ced324aac0
+  GIT_TAG ef5542fec4467fb53a4fec60180f55e83c0c9c8f
 )
 
 set(DIE_BUILD_AS_STATIC ON CACHE INTERNAL "")
