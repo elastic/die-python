85e86b86951c1d331a88735f182ef833314cceb9
Christophe Alladoum <christophe.alladoum@elastic.co>
2025-03-07 16:12:01 -0800

    check value in test

--- python/tests/test_regression.py
+++ python/tests/test_regression.py
@@ -5,15 +5,21 @@ import die
 
 TESTS_FOLDER = pathlib.Path(__file__).parent.absolute()
 DATA_FOLDER = TESTS_FOLDER / "data"
+DB_FOLDER = die.database_path / "db"
 
 
 def test_issue_48():
     # issue https://github.com/elastic/die-python/issues/28
     # pr https://github.com/elastic/die-python/pull/30
     fpath = DATA_FOLDER / "test.rar"
-    res = die.scan_file(fpath, die.ScanFlags.RESULT_AS_JSON)
+    res = die.scan_file(fpath, die.ScanFlags.RESULT_AS_JSON, str(DB_FOLDER))
     assert res
     js = json.loads(res)
     assert len(js["detects"]) == 1
-    assert js["detects"][0]["filetype"] != "Binary"  # would fail in 0.2.0
-    assert js["detects"][0]["filetype"] == "Archive: RAR"
+    assert js["detects"][0]["filetype"] == "Binary"
+
+    values = js["detects"][0]["values"]
+    assert len(values) > 0
+    value = values[0]
+    assert value["name"] == "RAR"
+    assert value["string"].startswith("Archive: RAR(")
